88a3a20a4d14edb635daa1a9a285789ee5d9c761
seeffff <jdephil@gmail.com>
2016-12-09 09:20:40 -0500

    Base layouts made

---
+++ app/src/main/java/com/newwesterndev/gpsalarm/AlarmListFragment.java
@@ -0,0 +1,25 @@
+package com.newwesterndev.gpsalarm;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AlarmListFragment extends Fragment {
+
+
+    public AlarmListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
